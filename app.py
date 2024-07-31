85489bf1e8c1d2e7e13acf3f13897e6bd58c74c4
Anthony Perez <anthony.perez@uni.pe>
2024-07-30 20:02:52 -0500

    feat: :sparkles: update sockets

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
+from flask_socketio import SocketIO, emit
+from flask import request
+from decouple import config
 from config import configuration
 from src import init_app
-from decouple import config
 
 configuration = configuration['development']
 app = init_app(configuration)
 
+socketio = SocketIO(app, cors_allowed_origins=config("FRONTEND_URL"))
+
+
+@socketio.on('message')
+def handle_message(data):
+    current_socket_id = request.sid
+    body = {"data": data, "id": current_socket_id}
+    emit('message', body, broadcast=True)
+    print('received message: ' + data)
+
+
 if __name__ == '__main__':
-    app.run(port=config("PORT"))
+    socketio.run(app)
--- src/routes/IndexRoutes.py
+++ src/routes/IndexRoutes.py
@@ -1,10 +1,8 @@
 from flask import Blueprint, jsonify, request
-from decouple import config
 
 main = Blueprint('index_blueprint', __name__)
 
 
 @main.route('/')
 def index():
-    print(config('PORT')),
-    return 'Hello World!'
+    return 'Hello World 2!'
