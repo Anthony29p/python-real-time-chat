109712dde033dbad00eaf22de29ccd12bed4a6cf
Anthony Perez <anthony.perez@uni.pe>
2024-08-07 23:12:41 -0500

    fix: :bug: update redirect url config

--- src/services/AuthService.py
+++ src/services/AuthService.py
@@ -1,13 +1,16 @@
 from google_auth_oauthlib.flow import Flow
 from flask import session
+from decouple import config
 import os
 import pathlib
 
 client_secrets_file = os.path.join(pathlib.Path(__file__).parent.parent.parent, "client_secret.json")
+
+
 flow = Flow.from_client_secrets_file(
    client_secrets_file=client_secrets_file,
    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-   redirect_uri="http://localhost:5000/auth/oauth"
+   redirect_uri="{}/auth/oauth".format(config("BACKEND_URL"))
 )
 
 
